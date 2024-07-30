b0b3bbe93ba2bafd89a4ffd11d3f68f57f605f6b
T-J05 <bernardinotoledo6@gmail.com>
2024-07-30 12:57:37 -0400

    Add files via upload
    Actualizar registros: Escribe una consulta SQL para actualizar el nombre de un usuario específico en la tabla Usuarios.

---
+++ reto20.sql
@@ -0,0 +1,4 @@
+-- 20 Actualizar registros: Escribe una consulta SQL para actualizar el nombre de un usuario específico en la tabla Usuarios.
+UPDATE Usuarios
+Set nombre = 'Jose'
+WHERE nombre = 'Juan';
