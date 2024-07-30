63d39d3e686ca08b9b3b58f51f9a6f9fd4d09475
T-J05 <bernardinotoledo6@gmail.com>
2024-07-30 12:24:16 -0400

    Add files via upload
    Recursión Factorial: Implementa una función recursiva para calcular el factorial de un número pequeño (por ejemplo, 5).

---
+++ reto9.py
@@ -0,0 +1,10 @@
+#Recursión Factorial: Implementa una función recursiva para calcular el factorial de un número pequeño (por ejemplo, 5).
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+resultado = factorial(5)
+print(f'El factorial de 5 es: {resultado}')  
